80b28bd2285534f9b3c02007b31538b1a09e76ee
TerraMint <studentbreus@gmail.com>
2026-08-26 21:55:09 +0200

    feat(ex00): add canon for ScalarConverter

---
+++ ex00/inc/ScalarConverter.hpp
@@ -0,0 +1,8 @@
+class ScalarConverter {
+	private:
+		ScalarConverter(void);
+		ScalarConverter(const ScalarConverter& other);
+		ScalarConverter& operator=(const ScalarConverter& other);
+		~ScalarConverter(void);
+	public:
+};
---
+++ ex00/src/ScalarConverter.cpp
@@ -0,0 +1,22 @@
+#include "../inc/ScalarConverter.hpp"
+#include <string>
+ScalarConverter::ScalarConverter(void) {
+	
+}
+
+ScalarConverter::ScalarConverter(const ScalarConverter& other) {
+	*this = other;
+}
+
+ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other) {
+	if (this != &other) {
+		
+	}
+	return *this;
+}
+
+			
+
+ScalarConverter::~ScalarConverter(void) {
+}
+
