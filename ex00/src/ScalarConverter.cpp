70d5107ef0bb05c70bcd3a0449044f6c85d1c786
TerraMint <studentbreus@gmail.com>
2026-08-26 21:56:03 +0200

    feat(ex00): added findtype() helper function for convert()

--- ex00/src/ScalarConverter.cpp
+++ ex00/src/ScalarConverter.cpp
@@ -3,6 +3,7 @@
 #include <stdexcept>
 #include <iostream>
 #include <regex>
+
 ScalarConverter::ScalarConverter(void) {
 	
 }
@@ -18,6 +19,20 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other) {
 	return *this;
 }
 
+double findType(std::string str)
+{
+	if (std::regex_match(str, std::regex("[\\+-]?[0-9]+")))
+	{
+			//integer.
+		try {
+			int i = std::stoi(str);
+			return (static_cast<double>(i));
+			}
+		catch (const std::out_of_range& e) {
+            throw std::invalid_argument("Integer overflow/out of range: " + str);
+        }}
+	throw std::invalid_argument("Invalid argument: " + str);
+}
 			
 
 ScalarConverter::~ScalarConverter(void) {
